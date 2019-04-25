c6b970e071f9587322a7c5157fc5761db12bfcb7
ANDREI RUPERTTI CAMARGO DE OLIVEIRA <17204023@pucrs.br>
2019-04-24 22:17:59 -0300

    add list methods to my list

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		MyList<Integer> list = new MyList<>();
-		list.add(50);
+		System.out.println("\n///////////\n\n");
+		MyList<String> list = new MyList<>();
+		list.add("Olá");
+		list.add("opa");
+		list.add(0, "Adiciona no inicio");
+		list.add("Meio merda");
+		list.remove("Olá");
+		System.out.println(list.toString());
+		System.out.println("\n///////////\n\n");
 	}
 }
--- MyList.java
+++ MyList.java
@@ -1,3 +1,5 @@
+import java.util.*;
+// implements ListTAD
 public class MyList<T> {
 	private final int initialSize = 500;
 	private T[] list;
@@ -7,18 +9,91 @@ public class MyList<T> {
 		this.list = (T[]) new Object[initialSize];
 	}
 	
-	public void add(T value) {
-		this.list[size] = value;
-		size++;
-	}
-	
-	public void toString() {
-		System.out.print("[");
+	public String toString() {
+		StringBuilder buffer = new StringBuilder(); 
+        
+		buffer.insert(0, "[");
 		for(int i=0; i< size; i++) {
-			System.out.print(this.list[i]);
-			System.out.print(",");
+			buffer.insert(buffer.length(), this.list[i]);
+			if (i != (size-1)) buffer.insert(buffer.length(), ",");
 		}
-		System.out.println("]");
+		buffer.insert(buffer.length(), "]");
+		return buffer.toString();
 	}
+
+	public void add(T element) {
+		this.list[this.size++] = element;
+	}
+
+	public void add(int index, T element) {
+		if (index > this.size) throw new ArrayIndexOutOfBoundsException();
+		this.list[index] = element;
+		if (index == this.size) this.size++;
+	}
+
+	public T remove(int pos) {
+		if (pos >= this.size) throw new ArrayIndexOutOfBoundsException();
+		T element = this.list[pos];
+		for (int i=pos; i < this.size; i++) {
+			this.list[i] = this.list[i+1];
+		}
+		this.size--;
+		return element;
+	}
+
+	public T remove(T element) {
+		for (int i=0; i < this.size; i++) {
+			if (this.list[i] == element) return this.remove(i);
+		}
+		return null;
+	}
+
+
+	public T get(int pos) {
+		if (pos >= this.size) throw new ArrayIndexOutOfBoundsException();		
+		return this.list[pos];
+	}
+
+
+	public void set(int index, T element) {
+		if (index > this.size) throw new ArrayIndexOutOfBoundsException();
+		this.list[index] = element;
+		if (index == this.size) this.size++;
+	}
+
+	// public T search(T element) {}
+
+	// public boolean isEmpty() {}
+
+	// public int size() {} 
+
+	// public int count(T element) {}
+
+	// public void clean() {}
+
+	// public Iterator<T> iterator() {}
+
+	// /**********
+	// * Métodos adicionais, para fins didáticos. 
+	// *********/
+
+	// public void addFirst(T element) {}
+
+
+	// public void addLast(T element) {}
+
+
+	// public T removeFirst() {}
+
+	// public T removeLast() {}
+
+	// public T getFirst() {
+
+	// }
+
+	// public T getLast() {
+
+	// }
+
 	
 }
